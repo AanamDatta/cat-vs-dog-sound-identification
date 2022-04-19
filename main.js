aa287ec73f8533e03eee4556c27394a386eea07a
AanamDatta <78989806+AanamDatta@users.noreply.github.com>
2022-04-18 20:11:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+ function startClassification() {
+    navigator.mediaDevices.getUserMedia({audio:true})
+classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/Xh7IKhkcZ/model.json",modelloaded)
+}
+function modelloaded(){
+    classifier.classify(gotResult)
+    console.log("model is loaded ")
+ 
+}
+var dog = 0
+var cat = 0 
+function gotResult(error,results) {
+    if (error) {
+       console.log("error") 
+    } else {
+      document.getElementById("detectedvoice").innerHTML= results[0].label  
+     
+      if (results[0].label=="cat") {
+        cat = cat + 1
+        document.getElementById("animal_image").src="cat.png"
+    } else if (results[0].label=="dog") {
+dog = dog + 1
+document.getElementById("animal_image").src="dog png.png"
+    }
+        
+    }
+   document.getElementById("amount_cat").innerHTML=cat
+   document.getElementById("amount_dog").innerHTML=dog
+}
+    
+
